5471a89674505d88d84c2bd9fd66c0d06dea1676
Pankaj Jangid <pkjvit@gmail.com>
2023-03-11 13:17:05 +0530

    Update ReverseLinkedList.kt

--- app/src/main/java/com/pkj/wow/dsa/leetcode_blind75/linked_list/ReverseLinkedList.kt
+++ app/src/main/java/com/pkj/wow/dsa/leetcode_blind75/linked_list/ReverseLinkedList.kt
@@ -9,7 +9,7 @@ output: 4->3->2->1->null
 
 
  */
-fun reverseList(root: ListNode<Int>?): ListNode<Int>?{
+fun reverseListIterative(root: ListNode<Int>?): ListNode<Int>?{
     var head = root
     var revHead:ListNode<Int>? = null
     var headNext:ListNode<Int>? = null
@@ -24,6 +24,21 @@ fun reverseList(root: ListNode<Int>?): ListNode<Int>?{
     return revHead
 }
 
+fun reverseList(head: ListNode<Int>?): ListNode<Int>? {
+
+    if(head == null || head.next == null){
+        return head
+    }
+
+    var newHead = reverseList(head.next)
+
+    // head -> 3, newHead(head.next) -> 4, head.next.next = null
+    head.next?.next = head // 4 -> 3
+    head.next = null // 3 -> null
+    // 4 -> 3 -> null
+    return newHead
+}
+
 fun main(){
     var head = ListNode(
                 value = 1,
@@ -35,8 +50,21 @@ fun main(){
                         )
                 )
     )
+    var head2 = ListNode(
+        value = 1,
+        next = ListNode(
+            value = 2,
+            next = ListNode(
+                value = 3,
+                next = ListNode(4)
+            )
+        )
+    )
     println(head)
-    println("reverse: "+reverseList(head))
+    println(head2)
+
+    println("reverse iteration: "+ reverseListIterative(head))
+    println("reverse recursive: "+ reverseList(head2))
 }
 
 
